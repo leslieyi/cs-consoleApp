576d6edfb8e4c1a0493c63b0b9e42e2ee6200983
leslieyi <leslieyi411@gmail.com>
2022-02-10 09:47:45 -0500

    app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int num2 = Int32.Parse(age);
+            int newAge = num2 + 5;
+            Console.WriteLine("Your age in 5 years is going to be " + newAge);
+        }
+    }
+}
